827ae13952b07153f1bacf56a16bb6041e3527df
bram <bram@bbflight.com>
2026-05-25 18:14:35 -0700

    (fix) group notifications running in foreground (issue #648)

--- android/src/main/kotlin/com/bbflight/background_downloader/Notifications.kt
+++ android/src/main/kotlin/com/bbflight/background_downloader/Notifications.kt
@@ -617,7 +617,7 @@ object NotificationService {
             stateChange = groupNotification.update(taskWorker.task, notificationType)
             groupNotifications[groupNotificationId] = groupNotification
         }
-        if (stateChange) {
+        if (stateChange || taskWorker.runInForeground) {
             // need to update the group notification
             taskWorker.notificationId = groupNotification.notificationId
             val hasError = groupNotification.hasError
